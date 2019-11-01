1f77ff80be4fbd1006ca8f36abceb099cf017017
JasmineChiehLi <jasmine_chieh_li@outlook.com>
2019-11-01 12:58:59 +0800

    add perfect match problem

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ For BUPT course usage.
   - [Brute-Force](#brute-force)
     - [0-1 Package Problem](#0-1-package-problem)
     - [TSP Problem](#tsp-problem)
+    - [Perfect Match Problem](#perfect-match-problem)
 
 ## Brute-Force  
 ### 0-1 Package Problem  
@@ -18,3 +19,8 @@ For BUPT course usage.
 - [TSP](source/tsp.py)  
 
 <img src='image/tsp.png' width=40% />  
+
+### Perfect Match Problem  
+- [perfect match](source/perfect_match.py)  
+
+<img src='image/match.png' />
---
+++ source/perfect_match.py
@@ -0,0 +1,22 @@
+men = ['Xavier', 'Yancey', 'Zeus']
+ladies = ['Amy', 'Bertha', 'Clare']
+man_preference = [[1, 2, 3],
+                  [2, 1, 3],
+                  [1, 2, 3]]
+lady_preference = [[2, 1, 3],
+                   [1, 2, 3],
+                   [1, 2, 3]]
+
+matches = [-1, -1, -1] 
+
+while -1 in matches:
+    for i, man in enumerate(men):
+        if matches[i] == -1:
+            # current man has no matching 
+            for j, lady in enumerate(ladies):
+                if not(j in matches) or (matches[j] > lady_preference[j][i]):
+                    matches[j] = i
+                    break
+
+for i, match in enumerate(matches):
+    print('{0} - {1}'.format(ladies[i], men[match]))
