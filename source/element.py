9cd1aa03b8d8c68c33e3c223233c0417f7d65251
JasmineChiehLi <jasmine_chieh_li@outlook.com>
2019-12-05 13:27:53 +0800

    finish trimming element.py

--- source/element.py
+++ source/element.py
@@ -8,8 +8,7 @@ class Solution:
         self.map = {}
         self.kth = kth
         self.answer = None
-        self.pos = None
-    
+
     def solve(self):
         if self.kth < 1 or self.kth > len(self.process):
             print('Illegal parameter {0}'.format(self.kth))
@@ -18,11 +17,12 @@ class Solution:
             # record original index
             self.update()
             self.answer = self.devide(0, len(self.process)-1, self.kth)
-            print('No.{0} Smallest of {1} is {2} at index {3}'.format(self.kth, self.all, self.answer, self.map[self.answer]))
+            print('No.{0} Smallest of {1} is {2} at index {3}'
+                    .format(self.kth, self.all, self.answer, self.map[self.answer]))
 
     def devide(self, l, r, k):
         if l >= r:
-            return self.process[min(l, r)]
+            return self.process[r]
 
         pivot = self.process[l]
         i = l
@@ -31,14 +31,12 @@ class Solution:
             while self.process[j] > pivot and i < j:
                 j -= 1
 
-            if i != j:
-                self.process[i] = self.process[j]
+            self.process[i] = self.process[j]
 
             while self.process[i] < pivot and i < j:
                 i += 1
             
-            if i != j:
-                self.process[j] = self.process[i]
+            self.process[j] = self.process[i]
 
             if i == j:
                 self.process[i] = pivot
